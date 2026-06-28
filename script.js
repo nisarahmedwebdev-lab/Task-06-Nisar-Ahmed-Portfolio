88d698530cb1d7ebfd37625d99fd6de76643ad34
nisarahmedwebdev-lab <nisarahmed.webdev@gmail.com>
2026-06-28 12:58:35 +0500

    update add new fatures

--- script.js
+++ script.js
@@ -183,3 +183,20 @@ document.getElementById("top").addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+
+const topBtn = document.getElementById('top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        topBtn.classList.add('show');
+    } else {
+        topBtn.classList.remove('show');
+    }
+});
+
+topBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
